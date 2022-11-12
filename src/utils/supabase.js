b63d4e23f2df6e970ca2a1b64c7995b036c2dc25
Simey de Klerk <13088589+simeydk@users.noreply.github.com>
2022-11-12 04:34:23 +0000

    Pending changes exported from your codespace

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-import 'dotenv/config'
-
 export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_API_KEY,
